e421895323d56fec55d5ab284a08c6b892090187
caiminming <caiminming@qiyi.com>
2018-11-09 16:03:36 +0800

    project init

---
+++ travel/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <div id="nav">
+            <router-link to="/">Home</router-link>
+        </div>
+        <!-- router-view 显示的是当前路由地址所对应的内容-->
+        <router-view/>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
