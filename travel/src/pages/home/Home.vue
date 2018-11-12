a93e3b599a26f8f1c65e3a74f463be504232a04a
caiminming <caiminming@qiyi.com>
2018-11-12 19:38:29 +0800

    add weekend

--- travel/src/pages/home/Home.vue
+++ travel/src/pages/home/Home.vue
@@ -4,6 +4,7 @@
         <home-swiper></home-swiper>
         <home-icons></home-icons>
         <home-recommend></home-recommend>
+        <home-weekend></home-weekend>
     </div>
 </template>
 
@@ -12,15 +13,18 @@
     import HomeSwiper from './components/Swiper.vue'
     import HomeIcons from './components/Icons.vue'
     import HomeRecommend from './components/Recommend.vue'
+    import HomeWeekend from './components/Weekend.vue'
 
     export default {
         name: "Home",
         components: {
+            HomeWeekend,
             HomeRecommend,
             'home-header': HomeHeader,
             'home-swiper': HomeSwiper,
             'home-icons': HomeIcons,
-            'home-recommend': HomeRecommend
+            'home-recommend': HomeRecommend,
+            'home-weekend': HomeWeekend,
         }
     }
 </script>
---
+++ travel/src/pages/home/components/Weekend.vue
@@ -0,0 +1,88 @@
+<template>
+    <div>
+        <div class="title">周末去哪儿</div>
+        <ul>
+            <li
+                    class="item border-bottom"
+                    v-for="item in weekendList"
+                    :key="item.id"
+            >
+                <div class="item-img-wrapper">
+                    <img class="item-img" :src="item.imgUrl"/>
+                </div>
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeWeekend",
+        data: function () {
+            return {
+                weekendList: [
+                    {
+                        id: '0001',
+                        imgUrl: 'http://img1.qunarzz.com/sight/source/1511/ea/7f43191b747bb9.jpg_r_640x214_f58e829d.jpg',
+                        title: '上海去哪儿泡温泉',
+                        desc: '上海的朋友总会问，哪里可以泡温泉，哪里的温泉好呢'
+                    },
+                    {
+                        id: '0002',
+                        imgUrl: 'http://img1.qunarzz.com/sight/source/1509/41/0a74389a026adc.jpg_r_640x214_5ad7add7.jpg',
+                        title: '上海赏秋攻略',
+                        desc: '上海的秋天，哪里都是美美哒'
+                    },
+                    {
+                        id: '0003',
+                        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg',
+                        title: '上海必游TOP10',
+                        desc: '中西合璧，现代和传统各有各的精彩'
+                    },
+                    {
+                        id: '0004',
+                        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/7b/2a6b0ce087f0e5.jpg_r_640x214_5c76b115.jpg',
+                        title: '情迷博物馆',
+                        desc: '穿梭时光岁月寻找答案'
+                    },
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" type="text/stylus" scoped>
+    @import "~@/assets/styles/mixins.styl";
+
+    .title
+        line-height: .8rem
+        background: #eee
+        text-indent: .2rem
+        text-align: left
+
+    .item-img-wrapper
+        overflow: hidden
+        height: 0
+        padding-bottom: 37.9%
+
+        .item-img
+            width: 100%
+
+    .item-info
+        width 100%
+        padding: .1rem
+        text-align left
+
+        .item-title
+            line-height: .54rem
+            font-size: .32rem
+
+        .item-desc
+            line-height: .4rem
+            color: #ccc
+            ellipsis()
+</style>
