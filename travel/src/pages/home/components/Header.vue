b0afaca5ee7d8f1bbf48a9d6005a5eb10d6c0593
caiminming <caiminming@qiyi.com>
2018-11-12 11:27:52 +0800

    add header

--- travel/src/pages/home/components/Header.vue
+++ travel/src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
     <div class="header">
-        <div class="header-left">返回</div>
-        <div class="header-input">输入城市/景点/游玩主题</div>
-        <div class="header-right">城市</div>
+        <div class="header-left">
+            <div class="iconfont back-icon">&#xe624;</div>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe632;</span>
+            输入城市/景点/游玩主题
+        </div>
+        <div class="header-right">
+            城市
+            <span class="iconfont arrow-icon">&#xe64a;</span>
+        </div>
     </div>
 </template>
 
@@ -12,35 +20,46 @@
     }
 </script>
 
-<style scoped>
+<style lang="stylus" type="text/stylus" scoped>
+    /* 在css中引入其他的css，要使用@符号，需要在前面添加~ */
+    @import "~@/assets/styles/varibles.styl";
+
     /* 1rem = html font-size = 50px */
-    .header {
-        display: flex;
-        line-height: .88rem; /* 2倍图下43px */
-        background: #00bcd4;
-        color: #fff;
-    }
+    /* 1px = 0.02rem */
+    .header
+        display: flex
+        line-height: .86rem /* 2倍图下43px */
+        background: $bgColor
+        color: #fff
 
-    .header-left {
-        width: .64rem;
-        float: left;
-    }
+        .header-left
+            width: .64rem
+            float: left
 
-    .header-input {
-        flex: 1;
-        line-height: .64rem;
-        margin-top: .12rem;
-        margin-left: .2rem;
-        background: #fff;
-        border-radius: 0.1rem;
-        color: #ccc;
-    }
+            .back-icon
+                text-align: center
+                font-size: .4rem
 
-    .header-right {
-        width: 1.24rem;
-        float: right;
-        text-align: center;
-    }
+        .header-input
+            flex: 1
+            height: .64rem
+            line-height: .64rem
+            margin-top: .12rem
+            margin-left: .2rem
+            background: #fff
+            border-radius: 0.1rem
+            color: #ccc
+            text-align: left
+            padding-left: .2rem
+
+        .header-right
+            width: 1.24rem
+            float: right
+            text-align: center
+
+            .arrow-icon
+                font-size: .24rem
+                margin-left: -.04rem
 
 
 </style>
